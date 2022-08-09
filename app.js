5b5dd9a7c2b76e6171d0ca09e85975b17b41a5b4
Jennifer Joseph <josephjenny490@gmail.com>
2022-08-09 18:23:36 +0100

    9th august contribution

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+//const validator = require('validator');
 const getNotes = require('./notes');
-const add = require('./utils')
-const sum = add(4, -4);
+//const chalk = require ('chalk');
+// const add = require('./utils')
+// const sum = add(4, -4);
 
 console.log(getNotes());
+//console.log(validator.isURL('josephjenny@gmail.com'))
+//console.log(chalk.green('successful'));
