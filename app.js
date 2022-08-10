7062277013fc164baba17d3d8fbc30de9c7ed087
Jennifer Joseph <josephjenny490@gmail.com>
2022-08-10 20:19:57 +0100

    process arguments

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-//const validator = require('validator');
+const validator = require('validator');
 const getNotes = require('./notes');
 //const chalk = require ('chalk');
-// const add = require('./utils')
-// const sum = add(4, -4);
+ const add = require('./utils')
+ const sum = add(4, -4);
 
 console.log(getNotes());
-//console.log(validator.isURL('josephjenny@gmail.com'))
+console.log(validator.isURL('josephjenny@gmail.com'))
 //console.log(chalk.green('successful'));
+console.log(process.argv[2])
