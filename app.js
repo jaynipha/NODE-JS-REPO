137da077913761d8280edb3ae784059bdef867fc
Jennifer Joseph <josephjenny490@gmail.com>
2022-08-14 18:13:11 +0100

    adding a note

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-//const validator = require('validator');
-const getNotes = require('./notes');
-const yargs = require('yargs');
-const { demandOption } = require('yargs');
- const command = process.argv[2]
-console.log(getNotes());
+const Notes = require('./notes');
+// const yargs = require('yargs');
+// const { demandOption } = require('yargs');
+//  const command = process.argv[2]
+
 //console.log(validator.isURL('josephjenny@gmail.com'))
 
 
@@ -15,63 +14,64 @@ console.log(getNotes());
 //CUSTOMIZE YARGS VERSION
 //yargs.version('1.0.2');
 
-//create notes app using add,remove, list, delete
-yargs.command({
-    command:"add",
-    describe:"add a new note here",
-    builder:{
-        title:{
-            describe:'note title',
-            demandOption: true,
-            type:'string',
+// //create notes app using add,remove, list, delete
+// yargs.command({
+//     command:"add",
+//     describe:"add a new note here",
+//     builder:{
+//         title:{
+//             describe:'note title',
+//             demandOption: true,
+//             type:'string',
             
 
-        },
+//         },
     
-    body:{
-        describe:'note body',
-        demandOption:'true',
-        type:'string',
+//     body:{
+//         describe:'note body',
+//         demandOption:'true',
+//         type:'string',
 
-    },
-},
-    handler: function(argv){
-        console.log('title:' + argv.title);
-        console.log('body:' + argv.body);
-    }
-},
-)
+//     },
+// },
+    // handler: function(argv){
+    //     console.log('title:' + argv.title);
+    //     console.log('body:' + argv.body);
+    // }
+//},
+//)
 
-yargs.command({
-    command:"remove",
-    describe:"remove a new note here",
-    handler: function(){
-        console.log('removing a new note')
-    }
-})
-
-
+// yargs.command({
+//     command:"remove",
+//     describe:"remove a new note here",
+//     handler: function(){
+//         console.log('removing a new note')
+//     }
+// })
 
 
-yargs.command({
-    command:"list",
-    describe:"list a new note here",
-    handler: function(){
-        console.log('listing all the notes here . .')
-    }
-})
 
 
-yargs.command({
-    command:"read",
-    describe:"read a note here",
-    handler: function(){
-        console.log('read your note')
+// yargs.command({
+//     command:"list",
+//     describe:"list a new note here",
+//     handler: function(){
+//         console.log('listing all the notes here . .')
+//     }
+// })
 
-    }
-})
 
-yargs.parse();
+// yargs.command({
+//     command:"read",
+//     describe:"read a note here",
+//     handler: function(){
+//         console.log('read your note')
 
-console.log(process.argv[2])
+//     }
+// })
+
+// yargs.parse();
+
+// console.log(process.argv[2])
 //console.log(yargs.argv);
+
--- notes.js
+++ notes.js
@@ -1,6 +1,40 @@
+const fs = require('fs');
+
 const getNotes = function getNotes(){
 
     return 'your notes . . .'
 }
+const addNotes = function(title, body){
+ const notes = loadNotes();
+
+ notes.push({
+    title:'title',
+    body:'body'
+ })
+ console.log(notes);
+
+}
+saveNotes(notes);
+const saveNotes = function(notes){
+ const dataJson = JSON.stringify(notes);
+ fs.writeFileSync('notes.Json', dataJson);
+}
+    const loadNotes = function(){
+        try{
+            const dataBuffer = fs.readFileSync('notes.JSON');
+            const dataJson = dataBuffer.toString()
+            return JSON.parse(dataJson)
+        }    
+        catch (e) {
+        return[]
+       
+        }
+    }
+
+module.exports = {
+    getNotes : 'getNotes',
+    addNotes : 'addNotes'
+}
+
 
 module.exports = getNotes;
