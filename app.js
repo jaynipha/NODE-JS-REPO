a29a7eb1d676385d82a9058468878293fc23dec7
Jennifer Joseph <josephjenny490@gmail.com>
2022-08-19 22:12:15 +0100

    com

--- app.js
+++ app.js
@@ -106,3 +106,8 @@ setTimeout(() => {
 setTimeout(()=>{
     console.log('0 second timer')
 },2000);
+console.log('starting')
+
+setTimeout(() => {
+    console.log('5 seconds have passed')
+}, 5000);
