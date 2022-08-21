636783a20181a65fd9094b8fbd4375259a6dcead
Jennifer Joseph <josephjenny490@gmail.com>
2022-08-21 19:18:48 +0100

    decentralized project folder

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,27 +1,14 @@
-console.log('starting')
+const request = require ('request');
 
-setTimeout(() => {
-    console.log('5 seconds have passed')
-}, 5000);
+const url = "http://api.weatherstack.com/current?access_key=a0968ee011951dd696c577f843c5dad6&query=37.8267,-122/4233units=f"
 
-setTimeout(()=>{
-    console.log('0 second timer')
-},2000);
+request({url:url, json:true}, (error, response) => {
+ //console.log(response)
+ //const data = JSON.parse(response.body)
+ console.log('it is currently '+ response.body.current.temperature + ' degrees outside. there is a ' 
+ + response.body.current.feelslike + ' chance of rain');
+});
 
-console.log('starting')
-
-setTimeout(() => {
-    console.log('5 seconds have passed')
-}, 5000);
-
-setTimeout(()=>{
-    console.log('0 second timer')
-},2000);
-
-
-
-
-
-
-
-console.log('stopping')
+const geoCodeUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamFpeWtuZWVwaGFyciIsImEiOiJjbDczY3ZiY20wY2phM3ByMXJoamR3YzhqIn0.uKEs6wbQo3Lkr3a9a7xrGw"
+request(geoCodeUrl:geoCodeUrl, json:t)
+    
